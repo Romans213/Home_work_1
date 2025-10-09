be8568e22fe5e70eed9d03e7b5aad223ae44a8aa
Romans213 <lorgen808@gmail.com>
2025-10-09 13:36:25 +0300

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,20 @@
+# print("Hello World")
+# # print('test')
+# print('qqq')
+
+'''
+:param
+greeting = "Hello World"
+print(greeting)
+print(greeting.upper())
+print(greeting.lower())
+'''
+
+# \n перенесення на новий рядок
+# print("Hello \n World")
+# \t -> 4 Пробіли (Табуляція)
+# \ -> Дзеркалення (якщо потрібно службовий символ зробити друкованим)
+
+print("Hello World!", sep ="", end ="-" )
+# print('test')
+print('qqq', "wfsdkf", "fsdfsdda")
